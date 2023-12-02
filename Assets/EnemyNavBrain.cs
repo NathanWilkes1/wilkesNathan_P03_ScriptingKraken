e53ca692ad9ca4e1df24c4baf516f1598ced0595
Nathan Wilkes <nathanallanwilkes@gmail.com>
2023-12-02 03:31:15 -0600

    made the basic navmesh and navbrain for the enemy.
    the script schould be something attachable ny the end without needing to be opened. a basic approach is set up but this stage is mostly for learning how navmesh worked.

---
+++ Assets/EnemyNavBrain.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavBrain : MonoBehaviour
+{
+    [SerializeField] private Transform Target;
+    private NavMeshAgent navMeshAgent;
+    private Transform Player;
+    
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        navMeshAgent.destination = Target.position;
+    }
+
+}
