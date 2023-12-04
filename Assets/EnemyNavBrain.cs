d274f41f5a1cbe1662324763c2403f561b10ca3f
Nathan Wilkes <nathanallanwilkes@gmail.com>
2023-12-04 05:45:43 -0600

    Finished Tool
    set up the roam and gizmos. also made the Inspector more organized.

--- Assets/EnemyNavBrain.cs
+++ Assets/EnemyNavBrain.cs
@@ -9,8 +9,9 @@ public class EnemyNavBrain : MonoBehaviour
     private NavMeshAgent navMeshAgent;
     private Transform Player;
     private bool spotted = false;
+    private int patrolCounter = 0;
 
-    
+
 
     enum options_1 // your custom enumeration
     {
@@ -25,21 +26,33 @@ public class EnemyNavBrain : MonoBehaviour
         Approach,
         Roam
     };
-    
+    [Header("AI modes")]
     [SerializeField] options_1 Unseen;
     [SerializeField] options_2 Seen;
+    [Header("Vision Setting")]
     [SerializeField] [Range(1, 360)] float visionCone = 45f;
     [SerializeField] float visionRange = 10f;
+    [SerializeField] [Tooltip("when checked, the AI will be able to look through walls to find their target")] private bool XrayVision = false;
+    [Header("Movement")]
     [SerializeField] float MovementSpeed = 1f;
-    private float stop = 1;
+    [SerializeField] [Tooltip("Controls how close the AI gets to their destination")] private float stop = 3;
     private options_1 mode;
+    [SerializeField] [Tooltip("Used for the patrol mode in options 1. place empty gameobjects with transforms here to move between")] private Transform[] PatrolPoints;
+    
 
-
-    private void Awake()
+    private void OnDrawGizmosSelected()
     {
-       
-        
+        var coneGizmoRay = this.gameObject.transform.position;
+        Gizmos.DrawWireSphere(this.transform.position, visionRange);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0, visionCone+ this.gameObject.transform.rotation.eulerAngles.y, 0) * new Vector3(0, 0, visionRange));
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0, visionCone*-1+ this.gameObject.transform.rotation.eulerAngles.y, 0) * new Vector3(0, 0, visionRange));
+
+
+
     }
+
+
+
 
     // Start is called before the first frame update
     private void Start()
@@ -48,6 +61,10 @@ public class EnemyNavBrain : MonoBehaviour
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.stoppingDistance = 2;
         navMeshAgent.speed = MovementSpeed;
+        if(Unseen == options_1.Patrol)
+        {
+            navMeshAgent.SetDestination(PatrolPoints[0].position);
+        }
     }
 
     private void Update()
@@ -70,7 +87,7 @@ public class EnemyNavBrain : MonoBehaviour
             }
         else if (Unseen == options_1.Patrol)
             {
-
+                Patrolling();
             }
         else if (Unseen == options_1.Roam)
             {
@@ -99,7 +116,14 @@ public class EnemyNavBrain : MonoBehaviour
             }
             else if (Seen == options_2.Roam)
             {
-
+                if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+                {
+                    Vector3 newDestination;
+                    if (RandomPoint(transform.position, 10, out newDestination))
+                    {
+                        navMeshAgent.SetDestination(newDestination);
+                    }
+                }
             }
         }
      
@@ -107,10 +131,9 @@ public class EnemyNavBrain : MonoBehaviour
     }
 
     //roaming code Start
-    //This code pretty much comes straight from the unity API as it does exactly what i needed straight out of the box. Changing it would be inefficient.
     bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
-        
+        //this code finds a random point within a sphere defined by the range.
             Vector3 randomPoint = center + Random.insideUnitSphere * range;
             NavMeshHit hit;
             if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
@@ -122,26 +145,45 @@ public class EnemyNavBrain : MonoBehaviour
         return false;
     }
     //Roaming code End
+
+    //patrol code start
+    private void Patrolling()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            patrolCounter++;
+            if(patrolCounter == PatrolPoints.Length)
+            {
+                patrolCounter = 0;
+            }
+            navMeshAgent.SetDestination(PatrolPoints[patrolCounter].position);
+        }
+    }
+    //patrol code end
     
-    
-    //vision code.
+    //vision code start.
     private void SeekTarget() 
     {
+        //subtracting to vectors from each other gives the line/angle between them
         Vector3 direction = Target.transform.position - transform.position;
-        if (Mathf.Abs(Vector3.Angle(transform.forward, direction)) < visionCone && Vector3.Distance(Target.transform.position, gameObject.transform.position) < visionRange)  
+        //mathf.abs is used because i want the AI to look in the negative direction too and it's easier to code without negatives.
+        if (Mathf.Abs(Vector3.Angle(transform.forward, direction)) < visionCone && Vector3.Distance(Target.transform.position, gameObject.transform.position) < visionRange)
         {
 
             //I actually took this from my turret script back from project 2.
             ////Raycasting to make sure it's not behind a wall made sense for the AI.
-
+            if (XrayVision == true)
+            {
+                spotted = true;
+            }
+            else { 
             Vector3 rayStartPos = gameObject.transform.position;
             Vector3 rayDirection = Target.transform.position - transform.position;
 
-            Debug.DrawRay(rayStartPos, rayDirection * 30, Color.cyan, 5);
+            Debug.DrawRay(rayStartPos, rayDirection * 30, Color.cyan, 1);
             if (Physics.Raycast(rayStartPos, rayDirection, out RaycastHit hitInfo, 30, ~2))
             {
-                Debug.Log(hitInfo.collider.gameObject.layer);
-                Debug.Log(hitInfo.collider.gameObject);
+
 
                 if (hitInfo.collider.gameObject == Target.gameObject)
                 {
@@ -152,9 +194,11 @@ public class EnemyNavBrain : MonoBehaviour
                 }
                 else
                 {
-                    
+
                 }
             }
         }
+        }
     }
+    //vision code end
 }
